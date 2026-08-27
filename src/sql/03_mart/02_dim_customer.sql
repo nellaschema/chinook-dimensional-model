9a3b03f7be062f051678e26d8579960f045fb9cd
virnalee <315608576+virnalee@users.noreply.github.com>
2026-08-27 13:25:51 +0800

    Update 02_dim_customer.sql

--- src/sql/03_mart/02_dim_customer.sql
+++ src/sql/03_mart/02_dim_customer.sql
@@ -1,1 +1,13 @@
+-- Create the Customer Dimension
 
+CREATE OR REPLACE TABLE d5_mart.DimCustomer AS
+SELECT
+    CustomerId,
+    FirstName,
+    LastName,
+    Company,
+    City,
+    State,
+    Country,
+    PostalCode
+FROM d5_clean.cleaned_customer;
