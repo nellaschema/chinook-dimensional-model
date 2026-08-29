30772061977e909b253bda9fec5e72a46bc52e31
saraevcldn <137899614+saraevcldn@users.noreply.github.com>
2026-08-29 15:31:58 +0800

    Update 02_customer_spending_tiers.sql

--- src/sql/04_analytics/02_customer_spending_tiers.sql
+++ src/sql/04_analytics/02_customer_spending_tiers.sql
@@ -1,1 +1,29 @@
+-- Group customers into tiers (High, Medium, Low) based on their total spend — how many customers fall into each tier?
+-- Threshold is based on the Equal Distribution (Thirds) 
+-- Low: < $39 (below average)
+-- Medium: $39 - $43 (average to 90th percentile)
+-- High: > $43 (top 10%)
 
+CREATE OR REPLACE TABLE d5_analysis.customer_segmentation_by_spending_tier AS
+WITH customer_spending AS (
+  SELECT 
+    c.CustomerId,
+    c.CustomerName,
+    c.Country,
+    SUM(f.LineAmount) AS total_spend
+  FROM workspace.d5_mart.dimcustomer c
+  INNER JOIN workspace.d5_mart.factsales f ON c.CustomerId = f.CustomerId
+  GROUP BY c.CustomerId, c.CustomerName, c.Country
+)
+SELECT 
+  CustomerId,
+  CustomerName,
+  Country,
+  ROUND(total_spend, 2) AS total_spend,
+  CASE 
+    WHEN total_spend > 43 THEN 'High' 
+    WHEN total_spend >= 39 THEN 'Medium'
+    ELSE 'Low'
+  END AS spending_tier
+FROM customer_spending
+ORDER BY total_spend DESC
