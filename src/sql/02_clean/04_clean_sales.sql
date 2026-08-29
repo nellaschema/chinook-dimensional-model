ffcf9ab8289dbc064fbaa4a0e8d573fe96aa03d6
bmcpl <81463616+lustrousiana@users.noreply.github.com>
2026-08-29 15:34:04 +0800

    Refactor SQL to create cleaned media type table
    Updated the SQL script to create a cleaned media type table and removed the creation of several other clean tables. Adjusted the selection of columns to include only MediaTypeId and Name.

--- src/sql/02_clean/04_clean_sales.sql
+++ src/sql/02_clean/04_clean_sales.sql
@@ -1,83 +1,10 @@
 
--- SILVER TABLE CREATION
--- All source columns are retained in the Silver layer.
--- Source quality checks and issue investigations are documented separately in tests/source_checks.sql.
-
--- Modify/create remaining tables at the end of this query.
-
-
--- Create clean_track
--- Note:
--- Two Track records have missing UnitPrice values (TrackId 3475 and 3477). 
--- These values are retained as NULL because revenue analysis uses the transaction-level UnitPrice from InvoiceLine.
--- Therefore, the missing Track-level UnitPrice values do not affect the revenue analysis.
-
--- TrackId = 3477 has a valid transaction-level UnitPrice of 0.99 in InvoiceLine.
--- TrackId = 3475 has no corresponding InvoiceLine record.
---
-CREATE OR REPLACE TABLE workspace.d5_silver.clean_track AS
+CREATE OR REPLACE TABLE workspace.d5_clean.cleaned_mediatype AS(
 SELECT
-    TrackId,
-    Name,
-    AlbumId,
-    MediaTypeId,
-    GenreId,
-    Composer,
-    Milliseconds,
-    Bytes,
-    UnitPrice
-FROM workspace.d5_raw.raw_track;
+   MediaTypeId ,
+    Name 
+FROM workspace.d5_raw.raw_mediatype
 
--- Create clean_invoice
-CREATE OR REPLACE TABLE workspace.d5_silver.clean_invoice AS
-SELECT
-    InvoiceId,
-    CustomerId,
-    InvoiceDate,
-    BillingAddress,
-    BillingCity,
-    BillingState,
-    BillingCountry,
-    BillingPostalCode,
-    BillingPhone
-FROM workspace.d5_raw.raw_invoice;
-
--- Create clean_invoiceline
-CREATE OR REPLACE TABLE workspace.d5_silver.clean_invoiceline AS
-SELECT
-    InvoiceLineId,
-    InvoiceId,
-    TrackId,
-    UnitPrice,
-    Quantity
-FROM workspace.d5_raw.raw_invoiceline;
-
--- Create clean_genre
-CREATE OR REPLACE TABLE workspace.d5_silver.clean_genre AS
-SELECT
-    GenreId,
-    Name
-FROM workspace.d5_raw.raw_genre;
+)
 
 
---Count all rows from all tables
-SELECT 'clean_track' AS table_name, COUNT(*) AS row_count
-FROM workspace.d5_silver.clean_track
-
-UNION ALL
-
-SELECT 'clean_invoice' AS table_name,  COUNT(*) AS row_count
-FROM workspace.d5_silver.clean_invoice
-
-UNION ALL
-
-SELECT  'clean_invoiceline' AS table_name, COUNT(*) AS row_count
-FROM workspace.d5_silver.clean_invoiceline
-
-UNION ALL
-
-SELECT  'clean_genre' AS table_name, COUNT(*) AS row_count
-FROM workspace.d5_silver.clean_genre
-
-
--- Continue creating below.
